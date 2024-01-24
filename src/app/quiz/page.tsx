76ff5be0363a0ef12e44f3dd1676d26b67f476b4
GunvaldH <gunvald.harket@softcare.no>
2024-01-24 17:27:30 +0100

    collecting points

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -9,9 +9,21 @@ type FlagType = {
     name: string
 }
 
+type FlagTypeExt = {
+    url: string
+    countrycode: string
+    name: string,
+    ix: number
+}
+
 type QuizSet = {
-    quizFlags: FlagType[],
+    quizFlags: FlagTypeExt[],
     quizPos: number
+}
+
+type QuizResult = {
+    flagIndex: number,
+    answer: boolean
 }
 
 
@@ -22,9 +34,10 @@ const Quiz = () => {
     const [currentQuiz, setCurrentQuiz] = useState<QuizSet>();
     const [reveal, setReveal] = useState<boolean>(false);
     const [answerIx, setAnswerIx] = useState<number>(-1);
+    const [results, setResults] = useState<QuizResult[]>([]);
 
     const [countrycodes, setcountrycodes] = useState(null);
-    const [flags, setflags] = useState<FlagType[]>([]);
+    const [flags, setflags] = useState<FlagTypeExt[]>([]);
 
     useEffect(() => {
         fetch("https://flagcdn.com/no/codes.json")
@@ -34,17 +47,17 @@ const Quiz = () => {
     }, []
     )
 
-    const getNoUSStatesFlagList = (cc: object):FlagType[] => {
+    const getNoUSStatesFlagList = (cc: object):FlagTypeExt[] => {
         if (cc === undefined)
             return [];
         else
         {
-                var ret: FlagType[] = [];
+                var ret: FlagTypeExt[] = [];
                 var i:number = 0;
                 for (const [key, value] of Object.entries(cc)) 
                 {
                     if (!key.startsWith("us-")) 
-                        ret[i++] = {url: urlbase + key + ".svg", countrycode: key, name: value.toUpperCase()};
+                        ret[i++] = {url: urlbase + key + ".svg", countrycode: key, name: value.toUpperCase(), ix:i};
                     
                 }
                 return ret;
@@ -75,9 +88,9 @@ const Quiz = () => {
         //const newIx3 = Math.floor(drawRandomIx(flags.length));
         const correctPos = Math.floor(drawRandomIx(3));
         
-        const correctFlag:FlagType = flags[newIx];
-        const wrongFlag2:FlagType = flags[newIx2];
-        const wrongFlag3:FlagType = flags[newIx3];
+        const correctFlag:FlagTypeExt = flags[newIx];
+        const wrongFlag2:FlagTypeExt = flags[newIx2];
+        const wrongFlag3:FlagTypeExt = flags[newIx3];
 
         const nextQuiz:QuizSet = {quizFlags: [correctFlag, wrongFlag2, wrongFlag3], quizPos: correctPos};
         setReveal(false);
@@ -88,10 +101,11 @@ const Quiz = () => {
         setCurrentFlag(correctFlag);
     }
 
-    const onAnswerQuiz = (answerIx: number) => {
+    const onAnswerQuiz = (answerIx: number, quizPos:number, correctFlagIndex: number) => {
         if (!reveal){
             setReveal(true);
             setAnswerIx(answerIx);
+            setResults([...results, {flagIndex: correctFlagIndex, answer: (answerIx === quizPos)?true:false}]);
         }
     }
 
@@ -106,24 +120,33 @@ const Quiz = () => {
     }
 
     const presentAlternatives = (quiz: QuizSet|undefined) => {
-        const classN = "display-inline my-2 px-4 border text-wrap cursor-pointer";
+        const baseChoiceStyle:string = "my-2 p-4 text-wrap cursor-pointer rounded";
+        const Neutral:string = baseChoiceStyle + " display-inline border bg-slate-200";
+        const WrongAnswer:string = baseChoiceStyle + " bg-red-600"
+        const CorrectAnswerWhenWrong:string = baseChoiceStyle + " bg-green-300"
+        const CorrectAnswerWhenCorrect:string = baseChoiceStyle + " bg-green-500"
         //const classCorr = "my-1 px-2 text-wrap bg-green-500 cursor-pointer";
-        const ix = quiz?.quizPos;
+        const qPos = quiz?.quizPos;
+        const correctFlagIndex = quiz?.quizFlags[quiz?.quizPos].ix;
 
-        var lineTp: string[] = ["my-2 px-4 text-wrap cursor-pointer", "my-2 px-4 text-wrap cursor-pointer", "my-2 px-4 text-wrap cursor-pointer"]
-        if (answerIx === ix)
-            lineTp[ix] = "my-2 px-4 text-wrap bg-green-500 cursor-pointer"
+        let lineTp: string[] = [] 
+        if (answerIx === qPos) {
+            for (let i = 0; i<3; i++)
+                lineTp[i] = (i===answerIx)? CorrectAnswerWhenCorrect: Neutral;
+        }
         else
         {
-            (ix!== undefined) && (lineTp[ix] = "my-2 px-4 text-wrap bg-green-300 cursor-pointer");
-            (answerIx !== -1) && (lineTp[answerIx] = "my-2 px-4 text-wrap bg-red-600 cursor-pointer")
+            for (let i = 0; i<3; i++) {
+                lineTp[i] = ((qPos !== undefined) && (i === qPos))? CorrectAnswerWhenWrong :
+                (((answerIx !== -1) && (i === answerIx))? WrongAnswer: Neutral);
+            }
         }
 
         return (
             <>
-                <div onClick={() => onAnswerQuiz(0)} className={(reveal? lineTp[0]:classN)}>- {quiz?.quizFlags[0].name}</div>
-                <div onClick={() => onAnswerQuiz(1)} className={(reveal? lineTp[1]:classN)}>- {quiz?.quizFlags[1].name}</div>
-                <div onClick={() => onAnswerQuiz(2)} className={(reveal? lineTp[2]:classN)}>- {quiz?.quizFlags[2].name}</div>
+                <div onClick={() => onAnswerQuiz(0, quiz?.quizPos??-1, correctFlagIndex??-1)} className={(reveal? lineTp[0]:Neutral)}>{quiz?.quizFlags[0].name}</div>
+                <div onClick={() => onAnswerQuiz(1, quiz?.quizPos??-1, correctFlagIndex??-1)} className={(reveal? lineTp[1]:Neutral)}>{quiz?.quizFlags[1].name}</div>
+                <div onClick={() => onAnswerQuiz(2, quiz?.quizPos??-1, correctFlagIndex??-1)} className={(reveal? lineTp[2]:Neutral)}>{quiz?.quizFlags[2].name}</div>
             </>
         )
     }
@@ -134,11 +157,14 @@ const Quiz = () => {
             <div className="mx-auto w-80">
                 <div className="flex flex-col justify-items-center place-items-stretch">
                 {(currentQuiz) && presentFlag(currentQuiz)}
-                    <div className="mx-4 my-2">Velg alternativ:</div>
+                    <div className="mx-4 mt-6">Velg alternativ:</div>
                     {(currentQuiz) && presentAlternatives(currentQuiz)}
                     <button className="m-3 bg-blue-500 hover:bg-blue-700  text-white py-2 px-4 rounded" 
                         onClick={selectNextQuiz}>{(currentQuiz)?"Neste":"Sett i gang"} </button>
                 </div>
+                    <div className="flex flex-row wrap">
+                        {results.map((i, index)=> <div key={index}>{(i.answer)?"😊":"🤨"}</div>)}
+                    </div>
             </div>
         </div>
     )
